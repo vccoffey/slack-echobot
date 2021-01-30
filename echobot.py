84dc9a4ca7bdfa4a333a773b5b2bbc50f053fabe
Vern Coffey <59817868+vccoffey@users.noreply.github.com>
2021-01-30 15:17:07 -0800

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -5,18 +5,20 @@ from time import sleep
 from slack import WebClient
 import os
 
-token = os.environ.get('TOKEN', None) # found at https://api.slack.com/web#authentication
-print('found token',token[0:6],'...')
-# sc = SlackClient(token)
-sc = WebClient(token=token)
-if sc.rtm_connect() == True:
-  print('Connected.')
-  while True:
-    response = sc.rtm_read()
-    for part in response:
-      if part['type'] == 'message':
-        if part['text'].lower().startswith('echo: '):
-          sc.api_call("chat.postMessage", channel=part['channel'], text=part['text'][len('echo: '):], username='echobot', icon_emoji=':robot_face:')
-    sleep(1)
-else:
-  print('Connection Failed, invalid token?')
+slack_token = os.environ.get('TOKEN', None) # found at https://api.slack.com/web#authentication
+print('found token',slack_token[0:6],'...')
+
+client = WebClient(token=slack_token)
+
+# sc = WebClient(token=token)
+# if sc.rtm_connect() == True:
+#   print('Connected.')
+#   while True:
+#     response = sc.rtm_read()
+#     for part in response:
+#       if part['type'] == 'message':
+#         if part['text'].lower().startswith('echo: '):
+#           sc.api_call("chat.postMessage", channel=part['channel'], text=part['text'][len('echo: '):], username='echobot', icon_emoji=':robot_face:')
+#     sleep(1)
+# else:
+#   print('Connection Failed, invalid token?')
